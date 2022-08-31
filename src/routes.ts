60213257064b1f73165b74ba2d77800b706ce116
Komil Guliev <komil-guliev@mail.ru>
2022-08-31 13:11:15 +0300

    feat: products

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Request, Response } from 'express';
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from './controller/product.controller';
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from './controller/session.controller';
 import { createUserHandler } from './controller/user.controller';
 import requireUser from './middleware/requireUser';
 import validateResource from './middleware/validateResource';
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from './schema/product.schema';
 import { createSessionSchema } from './schema/session.schema';
 import { createUserSchema } from './schema/user.schema';
 
@@ -17,6 +19,12 @@ function routes(app: Express) {
 
     app.get('/api/sessions', requireUser, getUserSessionsHandler);
     app.delete('/api/sessions', requireUser, deleteSessionHandler);
+
+    // product
+    app.post('/api/products', [requireUser, validateResource(createProductSchema)], createProductHandler);
+    app.put('/api/products/:productId', [requireUser, validateResource(updateProductSchema)], updateProductHandler);
+    app.get('/api/products/:productId', validateResource(getProductSchema), getProductHandler);
+    app.delete('/api/products/:productId', [requireUser, validateResource(deleteProductSchema)], deleteProductHandler);
 
 
 }
